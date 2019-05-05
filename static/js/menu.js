e111152ea4278bac6070a8f9bd09c8e44c907724
Sean Kirmani <sean@kirmani.io>
2019-05-05 14:45:19 -0700

    Add javascript for menu

---
+++ static/js/menu.js
@@ -0,0 +1,12 @@
+/*
+ * menu.js
+ * Copyright (C) 2019 Sean Kirmani <sean@kirmani.io>
+ *
+ * Distributed under terms of the MIT license.
+ */
+$( "#logo2" ).click(function() {
+    $(".speech-bubble" ).toggleClass("active");
+    $('.menu-icon .top').toggleClass('active');
+    $('.menu-icon .middle').toggleClass('active');
+    $('.menu-icon .bottom').toggleClass('active');
+});
