32b9ec84fba8919bd4d55d6a89ed955e16b877c3
Sean Kirmani <sean@kirmani.io>
2019-05-05 15:21:53 -0700

    Add script for computing exact age

---
+++ static/js/age.js
@@ -0,0 +1,17 @@
+/*
+ * age.js
+ * Copyright (C) 2019 Sean Kirmani <sean@kirmani.io>
+ *
+ * Distributed under terms of the MIT license.
+ */
+(function(){
+  'use strict';
+  var oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
+  var firstDate = new Date(1995,03,01);
+  var secondDate = new Date();
+
+  var diffDays = Math.round(
+    Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay)));
+  document.getElementById('days').innerHTML = diffDays;
+  document.getElementById('age').innerHTML = Math.floor(diffDays / 365);
+})();
